926ddeff604410eccf7ef93a6d9d398212533c29
kunxin-chor <chorkunxin@yahoo.com>
2024-09-02 12:18:00 +0000

    Added first version of create and read products

---
+++ 07-abc-bookstore/Main.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main {
+
+    // display the menu and make sure the user enters between 1 to 3
+    public static int displayMenu() {
+       
+        while (true) {
+            System.out.println("Main Menu");
+            System.out.println("1. Add a product");
+            System.out.println("2. List all products");
+            System.out.println("3. Quit");
+    
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter choice: ");
+            int choice = sc.nextInt();
+            if (choice >= 1 && choice <= 3) {
+                return choice;
+            } else {
+                System.out.println("Invalid choice. Please choose between 1 to 3");
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+  
+        // We create an ArrayList that can store Product
+        // (when we create a class, that class is also considered as a data type)
+        ArrayList<Product> catalog =  new ArrayList<Product>(); 
+        do {
+            int choice =  displayMenu();
+            if (choice==1) {
+                System.out.println("Add new product");
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Enter name: ");
+                String name = sc.nextLine();
+                System.out.print("Enter price: ");
+                double price = sc.nextDouble();
+                sc.nextLine(); // get rid of the buffered \n
+                System.out.print("Enter SKU: ");
+                String sku = sc.nextLine();
+                Product newProduct = new Product(name, price, sku);
+                catalog.add(newProduct);
+            }
+            if (choice==2) {
+                System.out.println("List all products");
+                for (Product p : catalog) {
+                    System.out.println("Name: " + p.getName());
+                    System.out.println("Price: " + p.getPrice());
+                    System.out.println("SKU: " + p.getSku());
+                    System.out.println();
+                }
+            }
+            if (choice==3) {
+                break;
+            }
+        } while (true);
+     
+      
+
+    }
+}
---
+++ 07-abc-bookstore/Product.java
@@ -0,0 +1,57 @@
+// Product class is public-access, meaning
+// any other classes can use it
+public class Product {
+    private String name;
+    private double price;
+    private String sku;
+
+    // when a product is created
+    // set its name  "N/A"
+    // price to 0
+    // and SKU to "N/A"
+    public Product() {
+        this.name = "N/A";
+        this.price = 0;
+        this.sku = "N/A";
+    }
+
+    // overload constructors are different variants
+    // of constructors
+    public Product(String name, double price, String sku) {
+        this.name = name;
+        this.price = price;
+        this.sku = sku;
+    }
+
+    // only populate the name and sku properties in the object
+    public Product(String name, String sku) {
+        this.name = name;
+        this.sku = sku;
+        this.price = 0; // explictly state that we want 0 as the default value
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setSKU(String sku) {
+        this.sku = sku;
+    }
+
+}
